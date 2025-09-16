ee484a601924a8158c10fcb31227903194f08e8c
shabbirkhan <khanshabbir126@gmail.com>
2025-09-16 21:59:49 +0530

    New change done

--- src/com/Array/Practice.java
+++ src/com/Array/Practice.java
@@ -3,6 +3,7 @@ package com.Array;
 
 public class Practice {
 	
+	//remove Even element for array...
 	public static int[] removeEven(int arr[]) {
 		
 		int oddCount = 0;
@@ -34,11 +35,49 @@ public class Practice {
 		 }
 		 System.out.println();
 	}
+	
+	
+	//Reverse Array
+	static int[] ReverseArray(int number[],int start , int end)
+	{
+		while(start < end)
+		{
+			int temp = number[start];
+			number[start]=number[end];
+			number[end]=temp;
+			start ++;
+			end --;
+		}
+		
+		return number;
+	}
+	
+	//Minimum Value...
+	public static int minimumValue(int arr[])
+	{
+		int min=arr[0];
+		for(int i=0 ;i < arr.length ;i++)
+		{
+			if(arr[i] < min) //
+			{
+				min=arr[i];
+				System.out.println(arr[i]);
+			}
+		}
+		return min;
+	}
+	
 
 	public static void main(String[] args) {
-		int[] arr= {2,4,3,5,7,9,1,13,6};
+		int[] arr= {7,4,3,5,2,9,1,13,6};
 		
 		printArray(removeEven(arr));
+		System.out.print("orginal Array:- ");
+		printArray(arr);
+		System.out.print("Reverse Array:- ");
+		printArray(ReverseArray(arr,0,arr.length-1));
+		
+		System.out.println(minimumValue(arr));
 		
 	}
 }
