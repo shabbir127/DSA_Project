e17a89dd17b23b22e1eb7ce2b0c996fcbf1bba1b
shabbirkhan <khanshabbir126@gmail.com>
2025-09-29 17:40:43 +0530

    commit

--- src/com/SinglyLinkedList/PracticeLinkedList.java
+++ src/com/SinglyLinkedList/PracticeLinkedList.java
@@ -2,4 +2,33 @@ package com.SinglyLinkedList;
 
 public class PracticeLinkedList {
 
+	private ListNode head;
+	 
+	 private static class ListNode{
+		 private int data;
+		 private ListNode next;
+		 
+		 public ListNode(int data)
+		 {
+			 this.data=data;
+			 this.next=null;
+		 } 
+   
+	 }
+	 
+	 public static void main(String[] args) {
+		
+		 PracticeLinkedList pll=new PracticeLinkedList();
+		 pll.head=new ListNode(10);
+		 ListNode second=new ListNode(20);
+		 ListNode third=new ListNode(30);
+		 ListNode fourth=new ListNode(40);
+		 
+		 //now we will connect them together to from a chain.
+		 pll.head.next=second;
+		 second.next=third;
+		 third.next=fourth;
+		 
+		 
+	}
 }
