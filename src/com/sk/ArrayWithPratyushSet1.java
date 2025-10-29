c3c23fc0ddfc5f1f3f23683e2ea4b09f0ad403eb
shabbirkhan <khanshabbir126@gmail.com>
2025-10-29 22:51:44 +0530

    sum closest

--- src/com/sk/ArrayWithPratyushSet1.java
+++ src/com/sk/ArrayWithPratyushSet1.java
@@ -146,6 +146,12 @@ public class ArrayWithPratyushSet1 {
   	
 	}
 	
+	//#sum closest to target....
+	
+	
+	
+	
+	
 	//main
 	public static void main(String[] args) {
 		int[] arr= {1,1,1,2,2,2,3,3,3,3,4,4,5,6,6,19};
