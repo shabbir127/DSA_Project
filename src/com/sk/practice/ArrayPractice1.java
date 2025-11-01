05395c5b6c65c11bffa209e7756463429b4524b0
shabbirkhan <khanshabbir126@gmail.com>
2025-11-01 20:42:41 +0530

    TripletsEqualZero commit

---
+++ src/com/sk/practice/ArrayPractice1.java
@@ -0,0 +1,30 @@
+package com.sk.practice;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ArrayPractice1 {
+	
+	public static List<List<Integer>> TripletsEqualZero(int[] arr)
+	{
+		List<List<Integer>> result=new ArrayList<>();
+		
+		Arrays.sort(arr);
+		
+		//for to move i to arr.length-2....
+		
+		
+		
+		
+		
+		
+		
+		
+		return result;
+		
+		
+		
+	}
+
+}
