0c15846383f080e5b6a15e99376974ee9afb8bcd
shabbirkhan <khanshabbir126@gmail.com>
2025-11-01 20:49:12 +0530

    Reached to half way

--- src/com/sk/practice/ArrayPractice1.java
+++ src/com/sk/practice/ArrayPractice1.java
@@ -13,7 +13,29 @@ public class ArrayPractice1 {
 		Arrays.sort(arr);
 		
 		//for to move i to arr.length-2....
-		
+		for(int i=0 ;i < arr.length-2 ;i++)
+		{
+			if( i > 0  && arr[i] == arr[i-1])
+			{
+				continue;
+			}
+			
+			int left=0;
+			int right=arr.length-1;
+			
+			while(left < right)
+			{
+				int sum=arr[i]+arr[left]+arr[right];
+				
+				if(sum==0)
+				{
+					result.add(Arrays.asList(arr[i],arr[left],arr[right]));
+					
+					//skip if the value is duplicate for left and right..........
+				}
+				
+			}
+		}
 		
 		
 		
