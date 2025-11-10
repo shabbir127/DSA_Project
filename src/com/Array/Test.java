a9b39225844b55f14d6964deb89e185f73e7eb6a
shabbirkhan <khanshabbir126@gmail.com>
2025-11-10 23:29:32 +0530

    commmit

--- src/com/Array/Test.java
+++ src/com/Array/Test.java
@@ -25,9 +25,42 @@ public class Test {
 		
 	}
 	
+	
+	
+	//palindrome check whether the string with exclude the alphanumeric will be ignored.
+	public static boolean isPalindrome(String s)
+	{
+		int i=0;
+		int j=s.length() -1;
+		
+		while(i < j) 
+		{
+			int left=s.charAt(i);
+			int right=s.charAt(i);
+			
+			
+			if(!Character.isLetterOrDigit(left))
+			{
+				i=i++;
+			}
+			if(!Character.isLetterOrDigit(right))
+			{
+				j=j++;
+			}
+			if (Character.isLowerCase(left) != Character.isLowerCase(right)) {
+				return false;
+			}
+			
+			i++;
+			j++;
+		}
+		return true;
+	}
+	
 	public static void main(String[] args) {
 		String[] arr= {"l","e","v","e","l"};
 		pandilrome(arr);
+		
 		
 	}
 
