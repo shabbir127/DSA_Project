4ec9d700e05c9b2bd8c65072ba7ba7b1f49b8631
shabbirkhan <khanshabbir126@gmail.com>
2025-11-15 22:18:50 +0530

    pair less then target.

---
+++ src/com/Array/PairsLessThenTarget.java
@@ -0,0 +1,37 @@
+package com.Array;
+
+import java.util.List;
+
+public class PairsLessThenTarget {
+
+	public static int pairs(List<Integer> nums, int target)
+	{
+		int i=0;
+		int j=nums.size()-1;
+		
+		int count=0;
+		
+		while(i<j)
+		{
+			int sum=nums.get(i)+nums.get(j);
+			if(sum < target)
+			{
+				count=count + (j-i);
+				i++;				
+			}else {
+				j--;
+			}
+		}
+		
+		
+		return count;
+	}
+	
+	public static void main(String[] args) {
+	    List<Integer> nums = List.of(-1,1,2,3,1); 
+		int target = 2;
+		
+		int result=pairs(nums, target);
+		System.out.println("Result "+ result);
+	}
+}
