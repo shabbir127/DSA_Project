eae5c4efc12598e0b88756c1892ba27adb70dcd8
shabbirkhan <khanshabbir126@gmail.com>
2025-11-22 21:36:10 +0530

    functional interface...

---
+++ src/com/Java8/FunctionalInterfaceEx.java
@@ -0,0 +1,56 @@
+package com.Java8;
+
+
+public class FunctionalInterfaceEx implements Runnable{
+
+	
+	public static void main(String[] args) {
+		Test t1=()->{System.out.println("functional interface");};
+		
+		Test1 t2=(a,b)->{ System.out.println((a*b));};
+		
+		t2.add(12, 23);
+		t1.m1();
+		t1.m2();
+		Test.m3();
+		
+		//Thread operation 
+		FunctionalInterfaceEx fEx=new FunctionalInterfaceEx();
+		 Thread thread=new Thread(fEx);
+		 thread.start();
+	}
+
+	@Override
+	public void run() {
+	 
+		for(int i=0;i<5 ;i++)
+		{
+			System.out.print("Child thread.. ");
+		}
+		
+	}
+	
+}
+
+@FunctionalInterface
+interface Test{
+	
+	public void m1();
+	
+	public default void m2()
+	{
+		System.out.println("Hello, default method");
+	}
+	
+	public static void m3()
+	{
+		System.out.println("Hi, static block is here.....");
+	}	
+}
+
+
+@FunctionalInterface
+interface Test1{
+	
+	public void add(int a,int b);
+}
