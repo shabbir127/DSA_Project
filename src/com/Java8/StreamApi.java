e148bd6bd6f299a44535601b2f59bea321958327
shabbirkhan <khanshabbir126@gmail.com>
2025-11-28 22:02:59 +0530

    done

--- src/com/Java8/StreamApi.java
+++ src/com/Java8/StreamApi.java
@@ -21,16 +21,23 @@ public class StreamApi {
 		
 		List<Integer> marks=Arrays.asList(52,26,47,79,48,33,42,71);
 		
+		System.out.println("-----------------------Filter Function --------------------------");
 		List<Integer> filterList=list.stream().filter(i-> i%2==0).toList();
-		System.out.println("Divsible by 2 :-" + filterList);
+		System.out.println("Divsible by 2 :-" + filterList);// input=10
+		                                                    //output=0 to 10
 		
 		List<Integer> passed=marks.stream().filter(i-> i > 35).toList();
 		System.out.println("Passed Student Result :- "+ passed); 
 		
+		System.out.println("-----------------------MAP Function --------------------------");
 		List<Integer> graceMarks=marks.stream().filter(i-> i < 35).map(j-> j+5).filter(i -> i > 35).toList();
+		System.out.println("After add graceMarks "+ graceMarks);// input=10
+                                                                //output=10
 		
-		System.out.println("After add graceMarks "+ graceMarks);
 		
+		
+		
+		System.out.println("---------------------------------------- Working on Map with Stream API ----------------------------------------------------------------");
 		System.out.println("All Student list : "+ Student);
 	    Map<String, Integer> passedMap=Student.entrySet().stream().filter(i-> i.getValue() > 35).collect(Collectors.toMap(i-> i.getKey(),i->i.getValue() ));
 	    System.out.println("Student passed with grace marks list : "+passedMap);
@@ -38,6 +45,7 @@ public class StreamApi {
 	    Map<String, Integer> graceMap=Student.entrySet().stream().filter(i-> i.getValue() < 35).collect(Collectors.toMap(Map.Entry::getKey, i ->i.getValue() +5));
 	    System.out.println("Grace mark got and passed student list : "+graceMap);
 	    
+	    System.out.println("----------------------------------COUNT Function ------------------------------------");
 	    long CountStudent=marks.stream().filter(i-> i < 35).count();
 	    System.out.println("Total failed student:- "+ CountStudent);
 		
