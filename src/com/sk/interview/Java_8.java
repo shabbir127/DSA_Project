8ccb2126a382ef6cc1e1b8e43a9b8f1bac029f5f
shabbirkhan <khanshabbir126@gmail.com>
2025-12-12 22:46:27 +0530

    Total number of element is present

--- src/com/sk/interview/Java_8.java
+++ src/com/sk/interview/Java_8.java
@@ -7,8 +7,13 @@ public class Java_8 {
 	
 	public static void main(String[] args) {
 		
-		List<Integer> nums=Arrays.asList(10,22,34,5,100,13);
+		//first occourance 
+		List<Integer> nums=Arrays.asList(9,10,22,34,5,100,13);
 		nums.stream().findFirst().ifPresent(System.out::println);
+		
+		//total number of element is present
+		System.out.println( "Total number :- "+nums.stream().count());
+		nums.stream().filter(num -> num%2==0).forEach(System.out::println);
 		
 		
 	}
