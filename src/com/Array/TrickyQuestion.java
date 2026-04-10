469e55c7ec8e982ae962fc2c867358274efc1cf1
shabbirkhan <khanshabbir126@gmail.com>
2026-04-10 20:41:06 +0530

    commit

--- src/com/Array/TrickyQuestion.java
+++ src/com/Array/TrickyQuestion.java
@@ -1,59 +1,48 @@
 package com.Array;
 
-public class TrickyQuestion {
+public  class TrickyQuestion implements InterfaceA, InterfaceB {
 
-	static void  print()
-	{
-		
-		try {
-			M1();
-		} catch (Exception e) {
-			System.out.println("Exception handle in main catch");
-		}
-	}
+
+
 	
 	
-	static int tryblock()
-	{
-		int x=1;
-		try {
-			
-			System.out.println("try");
-			return x;
-		}finally {
-			x=2;
-			
-		}
-		
-	}
-	static void M1()
-	{
-		M2();
-	}
-	static void M2()
-	{
-		M3();
-	}
 	
-	static void M3()
-	{
-		int a=12/0;
-	}
+	
 	public static void main(String[] args) {
-		print(); 
-		System.out.println(tryblock());
 		
-		String string=null;
-		switch (string) {
-		case null:
-			System.out.println("null value:-");
-		default:
-			break;
-		 
-		}
 		
 	}
+
+
+	@Override
+	public void method1() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public int method1(int a) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+
+
+
+
 	
 	
+}
+
+
+
+interface InterfaceA
+{
+	void method1();
+}
+interface InterfaceB
+{
+	int method1(int a);
 }
 
